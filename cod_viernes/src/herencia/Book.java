2917ac0bb073e68f28cf21eb4b8d4b36328c7903
sudoagile <98191653+sudoagile@users.noreply.github.com>
2024-01-08 00:55:59 -0500

    update

---
+++ cod_viernes/src/herencia/Book.java
@@ -0,0 +1,11 @@
+package herencia;
+
+public class Book extends LibraryItem{
+
+    String autor;
+
+    public Book(int id, String titulo, boolean disponible, String autor) {
+        super(id, titulo, disponible);
+        this.autor = autor;
+    }
+}
